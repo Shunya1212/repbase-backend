af0bb0dc6c09e7b7f8f63b7e33548b27fdbf075e
Shunya1212 <shunya80112001@gmail.com>
2026-02-02 18:15:07 +0700

    add base test case

--- framework/tests.py
+++ framework/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django_currentuser.middleware import _set_current_user
 
-# Create your tests here.
+
+class BaseTestCase(TestCase):
+    def tearDown(self):
+        _set_current_user(None)
