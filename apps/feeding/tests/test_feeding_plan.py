d0ad7fac200b95a9f9756f6842b3d58590696794
Shunya1212 <shunya80112001@gmail.com>
2026-02-21 23:51:52 +0700

    update feeding plan api to include feeding item

--- apps/feeding/tests/test_feeding_plan.py
+++ apps/feeding/tests/test_feeding_plan.py
@@ -24,38 +24,65 @@ class FeedingPlanTestCase(BaseTestCase):
         plan = factories.FeedingPlanFactory()
         factories.FeedingPlanFactory()
 
-        response = self.client.get(reverse('feedinplan-detail', kwargs={'pk': plan.id}))
+        response = self.client.get(reverse('feedingplan-detail', kwargs={'pk': plan.id}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         self.assertEqual(response.data['id'], str(plan.id))
 
     def test_create_feeding_plan_should_success(self):
+        food_1 = factories.FoodFactory()
+        food_2 = factories.FoodFactory()
         response = self.client.post(reverse('feedingplan-list'), data={
             'name': 'plan_A',
             'frequency_days': 5,
-            'note': 'test note'
-        })
+            'note': 'test note',
+            'items': [
+                {'food': food_1.id, 'amount': 5},
+                {'food': food_2.id, 'amount': 10},
+            ]
+        },format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
         created_id = response.data['id']
         plan = models.FeedingPlan.objects.get(id=created_id)
+        items = plan.items.actives()
         self.assertEqual(plan.name, 'plan_A')
         self.assertEqual(plan.frequency_days, 5)
         self.assertEqual(plan.note, 'test note')
+        self.assertEqual(items.count(), 2)
+        self.assertEqual(items[0].food, food_2)
+        self.assertEqual(items[0].amount, 10)
+        self.assertEqual(items[1].food, food_1)
+        self.assertEqual(items[1].amount, 5)
 
     def test_update_feeding_plan_should_success(self):
         plan = factories.FeedingPlanFactory()
+        food_1 = factories.FoodFactory()
+        food_2 = factories.FoodFactory()
+        food_3 = factories.FoodFactory()
+        factories.FeedingPlanItemFactory(feeding_plan=plan, food=food_1)
+        factories.FeedingPlanItemFactory(feeding_plan=plan, food=food_2)
         response = self.client.patch(reverse('feedingplan-detail', kwargs={'pk': plan.id}), data={
             'name': 'plan_A',
             'frequency_days': 5,
-            'note': 'test note'
-        })
+            'note': 'test note',
+            'items': [
+                {'food': food_1.id, 'amount': 5},
+                {'food': food_3.id, 'amount': 15},
+            ]
+        },format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         plan.refresh_from_db()
+        items = plan.items.actives()
         self.assertEqual(plan.name, 'plan_A')
         self.assertEqual(plan.frequency_days, 5)
         self.assertEqual(plan.note, 'test note')
+        self.assertEqual(items.count(), 2)
+        self.assertEqual(items[0].food, food_3)
+        self.assertEqual(items[0].amount, 15)
+        self.assertEqual(items[1].food, food_1)
+        self.assertEqual(items[1].amount, 5)
 
     def test_delete_feeding_plan_should_success(self):
         plan = factories.FeedingPlanFactory()
